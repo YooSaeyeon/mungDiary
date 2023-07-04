76031617943f643040dd780be2bc0611faab05ff
SAEYEON YOO <126438004+YooSaeyeon@users.noreply.github.com>
2023-07-05 01:50:52 +0900

    Delete mungDiary directory

--- mungDiary/static/js/check.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const urlParams = new URLSearchParams(window.location.search);
-  const memoId = urlParams.get("id");
-  const memoDate = urlParams.get("date");
-  const memoContent = urlParams.get("content");
-
-  document.getElementById("date").innerHTML = memoDate;
-  document.getElementById("content").innerHTML = memoContent;
-});
